da0c93dc25cc9aacc87b5e52d88fdf65b7512b5c
Amuthu <sweety1.adb@gmail.com>
2019-05-09 11:56:24 +0200

    -

--- Geometry.java
+++ Geometry.java
@@ -1,9 +1,15 @@
 package com.company;
 
-public abstract class Geometry implements Forme{
+public abstract class Geometry {
 
 double x ;
 double y;
 double couleur;
+    abstract double area();
+    // only for 3d geometry
+    abstract double perimeter();
+    abstract double volume();
+    abstract void display();
+    abstract double compareformeto();
 
 }
